54671e38d3537aa2e15bcce8090228881a6f1115
marie langlois <marie.langlois2@gmail.com>
2023-02-21 13:47:45 +0100

    ok

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
 
+before_action :set_tasks, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -18,10 +19,26 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:name, :details)
+    params.require(:task).permit(:name, :details, :completed)
   end
 
+  def set_tasks
+    @task = Task.find(params[:id])
+  end
 end
